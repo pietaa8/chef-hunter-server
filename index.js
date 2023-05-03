ef461bea3319f6245251de22c17aee32c7edcfad
priyab99 <priya.bd.9911@gmail.com>
2023-05-04 00:28:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const port=process.env.PORT|| 5000;
+
+const chefs=require('./Data/chefs.json');
+
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+   res.send('chef is running')
+});
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+})
+
+app.listen(port,()=>{
+    console.log(`Chaf api is running:${port}`)
+})
